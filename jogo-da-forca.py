4f98ca6e1db57dfff522fdf9318bc780d4410510
Pedro Arthur <pedroarthurmelorodrigues@gmail.com>
2024-10-02 13:12:37 -0300

    Create jogo-da-forca.py

---
+++ jogo-da-forca.py
@@ -0,0 +1,61 @@
+print('Bem vindo ao Jogo da Forca')
+
+try:
+    # Configuração do jogo
+    stringA = input('Digite a Palavra Secreta: ').upper()
+    while not stringA.isalpha():  # Verifica se a palavra contém apenas letras
+        print("Erro: A palavra deve conter apenas letras.")
+        stringA = input('Digite a Palavra Secreta: ').upper()
+
+    while True:
+        try:
+            n_tentativas = int(input('Digite quantas tentativas: '))
+            if n_tentativas > 0:
+                break
+            else:
+                print("Erro: O número de tentativas deve ser positivo.")
+        except ValueError:
+            print("Erro: Digite um número válido.")
+
+    # Limpa a tela (funciona em terminais que suportam ANSI)
+    print("\x1b[2J\x1b[1;1H")
+
+    # Prepara a string oculta com sublinhados
+    stringB = ['_'] * len(stringA)
+
+    # Jogo
+    print(f'A palavra tem {len(stringA)} caracteres')
+
+    while n_tentativas > 0 and "_" in stringB:
+        entrada = input('Digite uma letra ou palavra: ').upper()
+
+        if len(entrada) == 1 and not entrada.isalpha():
+            print("Erro: Digite apenas letras.")
+            continue
+
+        if entrada == stringA:
+            stringB = list(stringA)  # Revela a palavra completa
+            break
+
+        if len(entrada) == 1 and entrada in stringA:
+            for i, letra in enumerate(stringA):
+                if letra == entrada:
+                    stringB[i] = entrada
+        else:
+            print("Letra ou palavra incorreta!")
+
+        n_tentativas -= 1
+        print(f'Você tem {n_tentativas} tentativas restantes')
+        print(' '.join(stringB))
+
+    if "_" not in stringB:
+        print('Parabéns, você acertou!')
+    else:
+        print(f'Que pena, esgotou as tentativas. A palavra correta era {stringA}.')
+
+    print('########################## Fim de Jogo ##########################')
+
+except KeyboardInterrupt:
+    print("\nJogo interrompido pelo usuário. Até a próxima!")
+except Exception as e:
+    print(f"Ocorreu um erro inesperado: {e}")
